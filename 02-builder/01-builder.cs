751e04de8e4cf2193718bbf2c1120a9af890144c
mooping <mongkonchai4412@gmail.com>
2018-08-14 13:47:34 +0700

    add builder

---
+++ 02-builder/01-builder.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Element = new List<HtmlElement>();
+        const int indentSize = 2;
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Text = text ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        string ToStringImpl(int indent) 
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indentSize * indent);
+            sb.AppendLine($"{i}<{Name}>");
+
+            if(!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach(var e in Element) 
+            {
+                sb.Append(e.ToStringImpl(indent + 1));
+            }
+            sb.AppendLine($"{i}</{Name}>");
+            return sb.ToString();
+        }
+
+        public override string ToString() 
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        readonly string rootName;
+        HtmlElement root = new HtmlElement("", "");
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public HtmlBuilder AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Element.Add(e);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement(rootName, "");
+        }
+    }
+
+    class Demo
+    {
+        public static void Main()
+        {
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "Hello").AddChild("li", "world");
+
+            Console.WriteLine(builder);
+        }
+    }
+}
---
+++ 02-builder/02-facade-builder.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace FacetedBuilder
+{
+
+    public class Person 
+    {
+        public string StreetAddress, Postcode, City;
+
+        public string CompanyName, Position;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
+    }
+
+    public class PersonBuilder 
+    {
+        protected Person person = new Person();
+
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+        public static implicit operator Person(PersonBuilder personBuilder)
+        {
+            return personBuilder.person;
+        }
+    }
+
+    public class PersonAddressBuilder: PersonBuilder
+    {
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostcode(string postcode)
+        {
+            person.Postcode = postcode;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
+        }
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int amount) 
+        {
+            person.AnnualIncome = amount;
+            return this;
+        }
+    }
+
+    class Demo
+    {
+        public static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            Person person = pb
+                .Lives.At("123 London Road")
+                      .In("London")
+                      .WithPostcode("SW12AC")
+                .Works.At("Google")
+                      .AsA("Enginner")
+                      .Earning(10);
+
+            Console.WriteLine(person);
+        }
+    }
+}
