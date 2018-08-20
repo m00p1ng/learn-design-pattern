4f2e7fd9be75a19e4eecdbd1cf73301be592f819
mooping <mongkonchai4412@gmail.com>
2018-08-20 19:15:22 +0700

    add bridge

---
+++ structure/bridge/01-bridge.cs
@@ -0,0 +1,73 @@
+using System;
+
+namespace Bridge
+{
+    public interface IRenderer
+    {
+        void RenderCircle(float radius);
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            Console.WriteLine($"Drawing a circle of radius {radius}");
+        }
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            Console.WriteLine($"Drawing pixels for circle with radius {radius}");
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+
+        protected Shape(IRenderer renderer)
+        {
+            this.renderer = renderer;
+        }
+
+        public abstract void Draw();
+        public abstract void Resize(float factor);
+    }
+
+    public class Circle : Shape
+    {
+        float radius;
+
+        public Circle(IRenderer renderer, float radius) : base(renderer)
+        {
+            this.radius = radius;
+        }
+
+        public override void Draw()
+        {
+            renderer.RenderCircle(radius);
+        }
+
+        public override void Resize(float factor)
+        {
+            radius *= factor;
+        }
+    }
+
+    class Demo
+    {
+        public static void Main()
+        {
+            //IRenderer renderer = new RasterRenderer();
+            var renderer = new VectorRenderer();
+            var circle = new Circle(renderer, 5);
+
+            circle.Draw();
+            circle.Resize(2);
+            circle.Draw();
+        }
+    }
+}
+
