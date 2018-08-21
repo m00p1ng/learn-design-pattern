751fad70c1af241eee6b45a7fe46d676fb36c80b
mooping <mongkonchai4412@gmail.com>
2018-08-21 19:32:54 +0700

    add proxy

---
+++ structure/proxy/01-protection-proxy.cs
@@ -0,0 +1,57 @@
+using System;
+
+namespace Proxy
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car being driven");
+        }
+    }
+
+    public class Driver
+    {
+        public int Age { get; set; }
+
+        public Driver(int age)
+        {
+            Age = age;
+        }
+    }
+
+    public class CarProxy : ICar
+    {
+        Car car = new Car();
+        Driver driver;
+
+        public CarProxy(Driver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (driver.Age >= 16)
+                car.Drive();
+            else
+            {
+                Console.WriteLine("Driver too young");
+            }
+        }
+    }
+
+    public class Demo
+    {
+        static void Main()
+        {
+            ICar car = new CarProxy(new Driver(12)); // 22
+            car.Drive();
+        }
+    }
+}
